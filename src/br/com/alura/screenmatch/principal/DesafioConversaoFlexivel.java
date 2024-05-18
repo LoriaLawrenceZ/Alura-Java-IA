16d4d855e7ff77dc6c935cf38d4f5a5a1ba63d39
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-05-18 17:49:27 -0300

    ALS-2 [feat][deserialization] Conversão flexivel com setLenient()
    Conversão flexível com GSON
    
    Instanciação do GSON para isso é diferente
    -
        - Quando for passar condições é utilizado o
        -  faz com que (resumidamente) seja possível receber valores que não existam no Java Object, ou, que o Java Object receba um jsonBodo que tenha informação faltante

--- src/br/com/alura/screenmatch/principal/DesafioConversaoFlexivel.java
+++ src/br/com/alura/screenmatch/principal/DesafioConversaoFlexivel.java
@@ -1,2 +1,18 @@
-package br.com.alura.screenmatch.principal;public class DesafioConversaoFlexivel {
+package br.com.alura.screenmatch.principal;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+record PessoaDesafioConversaoFlexivel(String nome, int idade, String cidade) {}
+
+public class DesafioConversaoFlexivel {
+
+    public static void main(String[] args) {
+        String jsonPessoa = "{\"nome\":\"Rodrigo\",\"cidade\":\"Brasília\"}";
+
+        Gson gson = new GsonBuilder().setLenient().create();
+        PessoaDesafioConversaoFlexivel pessoa = gson.fromJson(jsonPessoa, PessoaDesafioConversaoFlexivel.class);
+
+        System.out.println("Objeto Pessoa: " + pessoa);
+    }
 }
